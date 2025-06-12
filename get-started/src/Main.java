0b4e8c290c9790aa703e5a1dad263000967dc2a9
Cargnieli2016 <cargnieli2016@gmail.com>
2025-06-12 05:50:26 -0300

    argumentos arg

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
+        String msg = "Programando em ";
+        System.out.println(msg + args[0]);
+        System.out.println("Precisamos de uma " + args[1]);
 
 
     }
