a3479bbbc65ad1ac574fd2d8921ace39f342baba
Cargnieli2016 <cargnieli2016@gmail.com>
2025-06-15 07:26:29 -0300

    trabalhando com operadores

---
+++ operadores/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.printf("true  && true = %s\n", true && true);
+        System.out.printf("false && false = %s\n", false && false);
+        System.out.printf("true  && false = %s\n", true && false);
+        System.out.printf("false && true = %s\n", false && true);
+        System.out.println("+++++++++++++++++++++++\n");
+
+        System.out.printf("true  || true = %s\n", true || true);
+        System.out.printf("false || false = %s\n", false || false);
+        System.out.printf("true  || false = %s\n", true || false);
+        System.out.printf("false || false = %s\n", false || false);
+
+        System.out.println("+++++++++++++++++++++++\n");
+        System.out.printf("!true = %s\n", !true);
+        System.out.printf("!false = %s\n", !false);
+
+
+    }
+}
