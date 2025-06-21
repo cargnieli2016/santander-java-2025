641a74f2b3f34ad4d3e04ffb3d1c4edfaa58cc17
Cargnieli2016 <cargnieli2016@gmail.com>
2025-06-21 19:22:21 -0300

    par ou impar

---
+++ estruturaRepeticaoFor/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (var i = 0; i <= 20; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
